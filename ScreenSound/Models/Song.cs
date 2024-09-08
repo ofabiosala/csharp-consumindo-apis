376609a358a2d46c99e463072b0d957653940c94
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 14:38:22 -0300

    (feat): adição da propriedade "Year".

--- ScreenSound/Models/Song.cs
+++ ScreenSound/Models/Song.cs
@@ -13,6 +13,9 @@ internal class Song
     [JsonPropertyName("genre")]
     public string? Genre { get; set; }
 
+    [JsonPropertyName("year")]
+    public string? Year { get; set; }
+
     [JsonPropertyName("duration_ms")]
     public int Duration { get; set; }
 
@@ -29,6 +32,7 @@ internal class Song
         Console.WriteLine($"Artista: {Artist}");
         Console.WriteLine($"Música: {Name}");
         Console.WriteLine($"Gênero: {Genre}");
+        Console.WriteLine($"Ano: {Year}");
         Console.WriteLine($"Duração: {DurationInMinutes}");
     }
 }
@@ -37,5 +41,6 @@ internal class Song
 
 // Em C#, a anotação "JsonPropertyName" é usada para mapear propriedades de um objeto C# para nomes específicos de propriedades em JSON quando está serializando ou desserializando objetos JSON.
 // Esta anotação é especialmente útil quando o nome da propriedade em JSON não corresponde exatamente ao nome da propriedade na classe C#.
+// Caso o nome da propriedade na classe C# corresponder a propriedade em JSON não é necessário a utilização da anotação "JsonPropertyName". Lembrando que o "case sensitive" interfere.
 
 // O método "ToString("D2")" formata o número como um inteiro com pelo menos dois dígitos. Se o número for menor que 10, ele será preenchido com um zero à esquerda.
