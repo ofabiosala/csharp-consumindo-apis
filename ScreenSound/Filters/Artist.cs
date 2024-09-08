34cbe218d9259028ba2fa0f54659fbeb9ef746c7
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 13:37:03 -0300

    (feat): implementação do método "ByGenre" na classe "Artist".

--- ScreenSound/Filters/Artist.cs
+++ ScreenSound/Filters/Artist.cs
@@ -21,4 +21,14 @@ internal class Artist
             Console.WriteLine($"{artist}");
         }
     }
+
+    public static void ByGenre(List<Song> songs, string genre)
+    {
+        var artists = songs.Where(song => song.Genre!.Contains(genre)).Select(song => song.Artist).Distinct().ToList();
+        // var artists = songs.OrderBy(song => song.Artist).Where(song => song.Genre!.Contains(genre)).Select(song => song.Artist).Distinct().ToList();
+        foreach (var artist in artists)
+        {
+            Console.WriteLine($"{artist}");
+        }
+    }
 }
