b05da73a558dc3598fe474c2556fb5a1a1c16e76
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 14:37:00 -0300

    (refactor): melhorias na exibição de informações.

--- ScreenSound/Filters/Artist.cs
+++ ScreenSound/Filters/Artist.cs
@@ -1,34 +1,44 @@
-﻿using ScreenSound.Models;
+﻿using ModelSong = ScreenSound.Models.Song;
 
 namespace ScreenSound.Filters;
 
 internal class Artist
 {
-    public static void OrderByAsc(List<Song> songs)
+    public static void OrderByAsc(List<ModelSong> songs)
     {
         var artists = songs.OrderBy(song => song.Artist).Select(song => song.Artist).Distinct().ToList();
+
+        Console.WriteLine($"Artistas (A > Z):");
+
         foreach (var artist in artists)
         {
-            Console.WriteLine($"{artist}");
+            Console.WriteLine($"> {artist}");
         }
     }
 
-    public static void OrderByDesc(List<Song> songs)
+    public static void OrderByDesc(List<ModelSong> songs)
     {
         var artists = songs.OrderByDescending(song => song.Artist).Select(song => song.Artist).Distinct().ToList();
+
+        Console.WriteLine($"Artistas (Z > A):");
+
         foreach (var artist in artists)
         {
-            Console.WriteLine($"{artist}");
+            Console.WriteLine($"> {artist}");
         }
     }
 
-    public static void ByGenre(List<Song> songs, string genre)
+    public static void ByGenre(List<ModelSong> songs, string genre)
     {
         var artists = songs.Where(song => song.Genre!.Contains(genre)).Select(song => song.Artist).Distinct().ToList();
         // var artists = songs.OrderBy(song => song.Artist).Where(song => song.Genre!.Contains(genre)).Select(song => song.Artist).Distinct().ToList();
+
+        Console.WriteLine($"Gênero: {genre}");
+        Console.WriteLine($"Artistas:");
+
         foreach (var artist in artists)
         {
-            Console.WriteLine($"{artist}");
+            Console.WriteLine($"> {artist}");
         }
     }
 }
