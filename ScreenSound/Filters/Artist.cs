fc00af103500001e5168c216296c902680a4cfd2
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 11:43:24 -0300

    (feat): implementação da classe "Artist" em "Filtros".

---
+++ ScreenSound/Filters/Artist.cs
@@ -0,0 +1,24 @@
+﻿using ScreenSound.Models;
+
+namespace ScreenSound.Filters;
+
+internal class Artist
+{
+    public static void OrderByAsc(List<Song> songs)
+    {
+        var artists = songs.OrderBy(song => song.Artist).Select(song => song.Artist).Distinct().ToList();
+        foreach (var artist in artists)
+        {
+            Console.WriteLine($"{artist}");
+        }
+    }
+
+    public static void OrderByDesc(List<Song> songs)
+    {
+        var artists = songs.OrderByDescending(song => song.Artist).Select(song => song.Artist).Distinct().ToList();
+        foreach (var artist in artists)
+        {
+            Console.WriteLine($"{artist}");
+        }
+    }
+}
