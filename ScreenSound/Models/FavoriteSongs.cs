d45f691ee0a851c7f9cee7ddbba3615f5cb97e2a
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 16:41:08 -0300

    (feat): implementação da classe "FavoriteSong".

---
+++ ScreenSound/Models/FavoriteSongs.cs
@@ -0,0 +1,29 @@
+﻿using ScreenSound.Filters;
+
+namespace ScreenSound.Models;
+
+internal class FavoriteSongs
+{
+    public string Person { get; set; }
+    public List<Song> Songs { get; set; }
+
+    public FavoriteSongs(string person)
+    {
+        Person = person;
+        Songs = new List<Song>();
+    }
+
+    public void AddFavoriteSong(Song song)
+    {
+        Songs.Add(song);
+    }
+
+    public void ShowFavoriteSongs()
+    {
+        Console.WriteLine($"Músicas Favoritas de {Person}");
+        foreach (var song in Songs)
+        {
+            Console.WriteLine($"> {song.Artist} - {song.Name}");
+        }
+    }
+}
