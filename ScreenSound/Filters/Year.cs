14c7e7c0aca427382cac029ca47d9083799501e0
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 14:35:31 -0300

    (feat): implementação da classe "Year" em "Filtros".

---
+++ ScreenSound/Filters/Year.cs
@@ -0,0 +1,19 @@
+﻿using ModelSong = ScreenSound.Models.Song;
+
+namespace ScreenSound.Filters;
+
+internal class Year
+{
+    public static void ByYear(List<ModelSong> allSongs, string year)
+    {
+        var songsByYear = allSongs.Where(song => song.Year!.Equals(year)).ToList();
+
+        Console.WriteLine($"Ano: {year}");
+        Console.WriteLine($"Músicas:");
+
+        foreach (var song in songsByYear)
+        {
+            Console.WriteLine($"> {song.Artist} - {song.Name}");
+        }
+    }
+}
