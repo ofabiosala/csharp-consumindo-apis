a9779eba2cfd6b86a721ab32d2f634039cd1ce44
Fábio Sala <ofabiosala@gmail.com>
2024-09-08 14:35:20 -0300

    (feat): implementação da classe "Song" em "Filtros".

---
+++ ScreenSound/Filters/Song.cs
@@ -0,0 +1,19 @@
+﻿using ModelSong = ScreenSound.Models.Song;
+
+namespace ScreenSound.Filters;
+
+internal class Song
+{
+    public static void ByArtist(List<ModelSong> allSongs, string artist)
+    {
+        var songsByArtist = allSongs.Where(song => song.Artist!.Equals(artist)).ToList();
+
+        Console.WriteLine($"Artista: {artist}");
+        Console.WriteLine($"Músicas:");
+
+        foreach (var song in songsByArtist)
+        {
+            Console.WriteLine($"> {song.Name}");
+        }
+    }
+}
